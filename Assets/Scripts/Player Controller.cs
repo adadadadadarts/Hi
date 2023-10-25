31a72c27fdd7ae52645e965f7c5617a8197fd122
Yaren Çetinkaya <20050151001@ybu.edu.tr>
2023-10-25 21:59:50 +0300

    Basit şeyler

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//360 degree movement
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    public Vector2 moveDirection;
+    
+    void Update()
+    {
+        
+    }
+    
+    
+}
