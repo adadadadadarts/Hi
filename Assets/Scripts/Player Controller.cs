07a4acd27a16ff80178e35d945926ae67a7c2b59
JystXen <oglumoguz@hotmail.com>
2023-10-26 16:51:37 +0300

    Create: Movement

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 //360 degree movement
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
+    public float speed = 5f;
     public Vector2 moveDirection;
-    
+    public Rigidbody2D rb;
     void Update()
     {
-        
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
     }
-    
-    
+
+    void FixedUpdate()
+    {
+       rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+    }
+
+
 }
